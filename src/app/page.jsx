03db9c667beb9872170ca2a88a425ff73ab92054
yudhistirajalu <yudhistirajalu.jalu.yjh@gmail.com>
2025-11-04 14:47:27 +0700

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,8 @@
 import LandingPage from "./landingpage";
+import { PrismaClient } from 'app/generated-prisma-client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
+const prisma = new PrismaClient().$extends(withAccelerate())
 export default function Wfa() {
   return <LandingPage />;
 }
